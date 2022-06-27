5af18dfcc95ca39599ac9ca146e01215322f3018
benching <jordanlacandula@gmail.com>
2022-06-27 08:07:14 +0800

    allow contents to have subcontents

--- resources/js/components/sectionBasic.vue
+++ resources/js/components/sectionBasic.vue
@@ -2,6 +2,12 @@
     <section>
         <h2>{{title}}</h2>
         <p>{{content}}</p>
+        <div v-for="s in subcontent">
+            <h3 v-if="s.hasHeader">{{s.t}}</h3>
+            <span v-else>{{s.t}}</span>
+            <p v-if="s.isHtml" v-html="s.c"></p>
+            <p v-else="s.isHtml">{{s.c}}</p>
+        </div>
     </section>
 </template>
 
@@ -10,7 +16,8 @@
         name: "sectionBasic",
         props: {
             title: String,
-            content: String
+            content: String,
+            subcontent: Array
         }
     }
 </script>
