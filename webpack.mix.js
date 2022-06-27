874375788674da8f0218e65233e323369a915cfb
benching <jordanlacandula@gmail.com>
2022-06-28 06:18:32 +0800

    added tailwind css

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
+    .postCss('resources/css/app.css','public/css', [require('tailwindcss')])
     .browserSync('localhost:8000');
