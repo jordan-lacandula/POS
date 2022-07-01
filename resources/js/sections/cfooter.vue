1375d3673397f10bf9671a61c2b7459f5ac4016b
benching <jordanlacandula@gmail.com>
2022-07-02 05:27:28 +0800

    added footer content

--- resources/js/sections/cfooter.vue
+++ resources/js/sections/cfooter.vue
@@ -1,3 +1,26 @@
 <template>
-    <footer>Footer</footer>
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item">
+                <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/refunds">Refunds</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" to="/policy">Policy</router-link>
+            </li>
+        </ul>
+        <p class="text-center text-muted">© 2022 Company, Inc</p>
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    background-color: white;
+    color:black;
+}
+</style>
