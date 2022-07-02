c678cf7bf9d05ff018beb0d5277d799c2e6235c0
benching <jordanlacandula@gmail.com>
2022-07-03 01:44:14 +0800

    fitted footer to end of page

--- resources/js/sections/cfooter.vue
+++ resources/js/sections/cfooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="py-3 my-4">
+    <footer class="py-3">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item">
                 <router-link class="nav-link" to="/">Home</router-link>
